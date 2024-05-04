3421a95945765537e0f06fdb04df4f5de9a3b2b3
sahilhp18 <168840784+sahilhp18@users.noreply.github.com>
2024-05-04 16:04:11 -0400

    Update finalcode.py

--- finalcode.py
+++ finalcode.py
@@ -34,7 +34,7 @@ st.markdown("<h1 style='text-align: center; color:green; font-family: Times New 
 st.markdown("""<h1 style='text-align: center; font-family: "Times New Roman", Times, serif; white-space: nowrap; font-size: 16px;'>This web app visualizes data of Starbucks locations around the world.</h1>""", unsafe_allow_html=True)
 
 # [ST4]: SIDEBAR LOGO
-st.sidebar.image("1020x1024.png", width=280)
+st.sidebar.image("Starbuckslogo.png", width=280)
 
 # Giphy
 st.markdown("""
